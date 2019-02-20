0e8267416e82d1f6633562b3e248ee6f838ce468
Schlaud <46453628+Schlaud@users.noreply.github.com>
2019-02-20 11:53:37 -0600

    Update description.cpp

--- code/description.cpp
+++ code/description.cpp
@@ -18,7 +18,7 @@ string description::fillLife(string str){
 	string desc;
 	string temp;
 	general obj;
-	infile.open ("SalesDataP5.csv");
+	infile.open ("TextFiles/Formats/earlyLife.txt");
 	if (infile.is_open())
 	{
 		int   c=PLACEHOLDER_AMT_OF_TEMPLATES;
@@ -65,7 +65,51 @@ string description::fillLife(string str){
 string description::fillCharacteristics(string str){
 	// read in template
 	string desc;
+	string temp;
+	general obj;
+	infile.open ("TextFiles/Formats/characteristics.txt");
+	if (infile.is_open())
+	{
+		int   c=PLACEHOLDER_AMT_OF_TEMPLATES;
+		int val = rand() % c;
+		for(int i = 0;i<val;i++)
+		{
+			infile.ignore(256, "/n");
+		}
+		infile.getline(desc, 256, '/n');
+		infile.close();
+	}
+	else
+	{
+		cout << "Error opening file";
+	}
+	// write function to fill in template.
+	// use directory: TextFiles/Formats/earlyLife, and TextFiles/WordsToPlugIn/... for adjectives, participles, and nouns to plug in.
+	while(desc.find("$M")!=desc.npos())
+	{
+		desc.replace(desc.find("$M"), name.length(), name);
+	}
+	while(desc.find("%M")!=desc.npos())
+	{
+		temp = obj.randomName();
+		desc.replace(desc.find("%M"), temp.length(), temp);
+	}
+	while(desc.find("%V")!=desc.npos())
+	{
+		temp = obj.randomVerb();
+		desc.replace(desc.find("%V"), temp.length(), temp);
+	}
+	while(desc.find("%P")!=desc.npos())
+	{
+		temp = obj.randomParticiple();
+		desc.replace(desc.find("%P"), temp.length(), temp);
+	}
+	while(desc.find("%O")!=desc.npos())
+	{
+		temp = obj.randomNoun();
+		desc.replace(desc.find("%O"), temp.length(), temp);
+	}
+	return desc;
 	 // write function to fill in template. 
 	 // use directory TextFiles/Formats/characteristics for template directory, and TextFiles/WordsToPlugIn/... for adjectives, participles, and nouns to plug in.
-	return desc;
 }
