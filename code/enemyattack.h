c9cf320b342509348a7a3d9fe09fb1e3db8e779b
LucAlexander <46453661+LucAlexander@users.noreply.github.com>
2019-03-23 10:41:54 -0700

    Update enemyattack.h

--- code/enemyattack.h
+++ code/enemyattack.h
@@ -4,11 +4,11 @@
 #include <sstream>
 #include <fstream>
 using namespace std;
-class enemyAttack{
-	public: 
-		string name;
-		int damage; // 1-5
-		int reliability;//chance out of 10 that it will hit 30% to 100%
-		enemyAttack();
-		string randomName();
+class enemyAttack {
+public:
+	string name;
+	int damage; // 1-5
+	int reliability;//chance out of 10 that it will hit 30% to 100%
+	enemyAttack();
+	string randomName();
 };
