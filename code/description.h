a54a7696069e4ffc68fbf455b05c7e5d3775ac2b
LucAlexander <46453661+LucAlexander@users.noreply.github.com>
2019-03-23 10:37:48 -0700

    Update description.h

--- code/description.h
+++ code/description.h
@@ -4,15 +4,16 @@
 #include <sstream>
 #include <fstream>
 using namespace std;
-class description{
-	public: 
-		string earlyLife;
-		string characteristics;
-		string qualities;
-		string name;
-		//constructor
-		description(string name);
-		//template filler functions
-		string fillLife(string str); // early life
-		string fillCharacteristics(string str); // description
+class description {
+public:
+	string earlyLife;
+	string characteristics;
+	string qualities;
+	string name;
+	//constructor
+	description();
+	description(string name);
+	//template filler functions
+	string fillLife(); // early life
+	string fillCharacteristics(); // description
 };
