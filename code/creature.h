fd8f24e12b2850cdcbdbd1af374964503d737008
LucAlexander <46453661+LucAlexander@users.noreply.github.com>
2019-03-23 10:38:58 -0700

    Update creature.h

--- code/creature.h
+++ code/creature.h
@@ -3,35 +3,44 @@
 #include <stdlib.h>
 #include <sstream>
 #include <fstream>
+#include "enemyattack.H"
+#include "item.h"
+#include "description.h"
 using namespace std;
-class creature{
+class creature {
 	public:
 		int hp;
+		int max_hp;
 		string name;
 		int kills;
 		bool block;
+		bool canStrike;
 };
-class player : public creature{
-	friend enemy;
-	public:
-		item weapon;
-		player();
-		player(string choice);
-		void playerStrike(item& weapon, creature& target);
-		void playerDie();
-		string chooseName();
-		void printStats();
+class player : public creature {
+public:
+	int inventoryCount[4];
+	wstring inventorySlot[4];
+	item weapon;
+	player();
+	player(string choice);
+	void playerHeal(int hp, wchar_t *surface);
+	void playerStrike(item& weapon, creature *target, wchar_t *surface);
+	void playerDie(wchar_t *surface);
+	string chooseName();
+	void printStats();
+	void printStats(int line, wchar_t *surface);
 };
-class enemy : public creature{
-	friend player;
-	public: 
-		//attributes
-		description background;
-		enemyAttack att;
-		//functions
-		enemy(); // constructs hp, name
-		void printStats();
-		void enemyStrike(creature& pl, enemyAttack& att);
-		void enemyDie(creature& pl);
-		void willDrop(enemyAttack& att);
+class enemy : public creature {
+public:
+	//attributes
+	//description background;
+	enemyAttack att;
+	int potions;
+	//functions
+	enemy();
+	void printStats(int line, wchar_t *surface);
+	void enemyHeal(int hp, wchar_t *surface);
+	void enemyStrike(creature *pl, enemyAttack& att);
+	void enemyDie(creature& pl);
+	bool willDrop(enemyAttack& att);
 };
