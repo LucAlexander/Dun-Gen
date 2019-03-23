80ce7bde07cfc3e317d23d1b3ec7fdc945fc5f79
LucAlexander <46453661+LucAlexander@users.noreply.github.com>
2019-03-23 10:38:36 -0700

    Create conEngine.h

---
+++ conEngine.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <chrono>
+#include <cstdlib>
+#include <stdlib.h>
+#include <sstream>
+#include <time.h>
+#include <stdio.h>
+#include <Windows.h>
+class conEngine {
+	public:
+		void conPrint(int line, wstring word, wchar_t *screen);
+		void conPrintAdd(int line, wstring word, wchar_t *screen);
+		void conClear(wchar_t *screen);
+		wstring StringToWString(string s);
+};
