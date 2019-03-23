b6a1fdc2cb6abd1107501e0b4ce853c444c1a2b4
LucAlexander <46453661+LucAlexander@users.noreply.github.com>
2019-03-23 10:40:17 -0700

    Update item.cpp

--- code/item.cpp
+++ code/item.cpp
@@ -3,25 +3,44 @@
 #include <stdlib.h>
 #include <sstream>
 #include <fstream>
+#include "general.h" 
 #include "item.h"
 using namespace std;
 //constructor
-item::item(){
+item::item() {
 	name = itemName();
 	damage = ((rand() % 5) + 1);
-	reliability = ((rand() % 8)+3);
-	//deal with special
+	reliability = ((rand() % 8) + 3);
+	special = 0;
 }
-item::item(string n, int d, int r){
+item::item(string n, int d, int r) {
 	name = n;
 	damage = d;
 	reliability = r;
-	//deal with special
+	special = 0;
 }
 //functions
-string item::itemName(){
-	//reading through the file goes here
-	string name = "The" + randomAdjective() + randomNoun();
-	//function to generate name goes here
+string item::itemName() {
+	const char* dir1 = "./nouns.txt";
+	const char* dir2 = "./participleAdjectives.txt";
+	const char* dir4 = "./personalityAdjectives.txt";
+	const char* dir3 = "./descriptionAdjectives.txt";
+	general obj;
+	string name;
+	int choice = rand() % 3 + 1;
+	switch (choice) {
+		case 1:
+			name = "The " + obj.pullRandom(dir2) + " " + obj.pullRandom(dir1);
+		break;
+		case 2:
+			name = "The " + obj.pullRandom(dir3) + " " + obj.pullRandom(dir1);
+		break;
+		case 3:
+			name = "The " + obj.pullRandom(dir4) + " " + obj.pullRandom(dir1);
+		break;
+		default:
+			name = "The " + obj.pullRandom(dir4) + " " + obj.pullRandom(dir1);
+		break;
+	}
 	return name;
 }
