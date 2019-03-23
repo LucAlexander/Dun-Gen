030a6733584bf3a5df5d34b47812998301ea4293
LucAlexander <46453661+LucAlexander@users.noreply.github.com>
2019-03-23 10:40:51 -0700

    Update enemyattack.cpp

--- code/enemyattack.cpp
+++ code/enemyattack.cpp
@@ -4,15 +4,35 @@
 #include <sstream>
 #include <fstream>
 #include "enemyattack.h"
+#include "general.h"
 using namespace std;
 //constructor
-enemyAttack::enemyAttack(){
+enemyAttack::enemyAttack() {
 	damage = ((rand() % 5) + 1);
-	reliability = ((rand() % 8)+3);
+	reliability = ((rand() % 8) + 3);
 	name = randomName();
 }
-string enemyAttack::randomName(){
-	string str;
-	//write code to return a string in the same format as the players item name.
-	return str;
+string enemyAttack::randomName() {
+	const char* dir1 = "./nouns.txt";
+	const char* dir2 = "./participleAdjectives.txt";
+	const char* dir4 = "./personalityAdjectives.txt";
+	const char* dir3 = "./descriptionAdjectives.txt";
+	general obj;
+	string name;
+	int choice = rand() % 3 + 1;
+	switch (choice) {
+	case 1:
+		name = "The " + obj.pullRandom(dir2) + " " + obj.pullRandom(dir1);
+		break;
+	case 2:
+		name = "The " + obj.pullRandom(dir3) + " " + obj.pullRandom(dir1);
+		break;
+	case 3:
+		name = "The " + obj.pullRandom(dir4) + " " + obj.pullRandom(dir1);
+		break;
+	default:
+		name = "The " + obj.pullRandom(dir4) + " " + obj.pullRandom(dir1);
+		break;
+	}
+	return name;
 }
