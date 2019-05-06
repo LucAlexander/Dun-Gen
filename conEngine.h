30fb93b58efad8e36f7ea45aa6fb7dde00f626ae
LucAlexander <46453661+LucAlexander@users.noreply.github.com>
2019-05-05 20:44:49 -0700

    Update conEngine.h

--- conEngine.h
+++ conEngine.h
@@ -9,18 +9,22 @@
 #include <time.h>
 #include <stdio.h>
 #include <Windows.h>
-class conEngine {
-	public:
-		void conPrint(int line, wstring word, wchar_t *screen);
-		void conPrintAdd(int line, wstring word, wchar_t *screen);
-		void conClear(wchar_t *screen);
-		wstring StringToWString(string s);
-		wstring generateLevel(node* head, string seed);
-};
-class node{
+class node {
+public:
+	node();
 	node* left;
 	node* right;
 	wstring chunk;
-	void nodeAdd(wstring isaacsaididontneedthis);
+	void nodeAdd(node* head, wstring section);
 	node* parent;
+};
+class conEngine {
+public:
+	void conPrint(int line, wstring word, wchar_t *screen);
+	void conPrintAdd(int line, wstring word, wchar_t *screen);
+	void conClear(wchar_t *screen);
+	wstring StringToWString(string s);
+	wstring generateLevel(node* head);
+	void fillTree(node* head);
+	void setTree(node* tree);
 };
